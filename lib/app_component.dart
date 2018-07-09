31157cc554b72cd18ba58074f14d9d518cabc37b
PanupongDeve <5635512110@psu.ac.th>
2018-07-09 09:48:12 +0700

    first commit

---
+++ lib/app_component.dart
@@ -0,0 +1,12 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'src/hero.dart';
+@Component(
+  selector: 'my-app',
+  directives: [formDirectives],
+  templateUrl: 'app_component.html',
+)
+class AppComponent {
+  final title = 'Tour of Heroes';
+  Hero hero = Hero(1, 'Windstorm');
+}
