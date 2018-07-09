7fc3d09e37744872a33fdff8f3356c95204fbf69
PanupongDeve <5635512110@psu.ac.th>
2018-07-09 10:03:35 +0700

    update handle click

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,12 +1,18 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
+import 'src/mock_heros.dart';
+
 @Component(
   selector: 'my-app',
-  directives: [formDirectives],
+  directives: [coreDirectives, formDirectives],
   templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  Hero hero = Hero(1, 'Windstorm');
+  Hero selected;
+  List<Hero> heroes = mockHeroes;
+
+  void onSelect(Hero hero) => selected = hero;
 }
